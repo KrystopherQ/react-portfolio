888d4e05b5f7d76cf98803b9987febfd94b7ffcb
Krystopher Quintero <krystopher.q@gmail.com>
2022-06-12 14:57:09 -0700

    add about me

---
+++ src/Components/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function About() {
+    return (
+<div className="hero min-h-screen" style="background-image: url();">
+<div className="hero-overlay bg-opacity-60"></div>
+<div className="hero-content text-center text-neutral-content">
+  <div className="max-w-md">
+    <h1 className="mb-5 text-5xl font-bold">ABOUT ME</h1>
+    <p className="mb-5">I am an Iron worker currently in school at UC Berkeley Extension to become a full stack web developer. Welcome to my Portfolio.</p>
+  </div>
+</div>
+</div>
+ )
+}
+
+export default About;
