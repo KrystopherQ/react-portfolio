5baf012377e95464d178abe0c778523e6eb8db83
Krystopher Quintero <krystopher.q@gmail.com>
2022-06-12 14:38:51 -0700

    create footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer(){
+  return(
+    <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+    <div className="grid grid-flow-col gap-4">
+        <a className="link link-hover" href='https://github.com/KrystopherQ' target="_blank">GitHub</a>
+        <a className="link link-hover" href="https://www.linkedin.com/in/krystopher-quintero-955320238/" target="_blank">Linkedin</a>
+    </div>
+</footer>
+  )
+}
+
+export default Footer;
