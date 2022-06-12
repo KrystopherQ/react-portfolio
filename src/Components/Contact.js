7327c670a3d3909c7a570ac6aa7ccc1ee78f6016
Krystopher Quintero <krystopher.q@gmail.com>
2022-06-12 14:41:17 -0700

    create contact page

---
+++ src/Components/Contact.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>
+    <div className="hero min-h-screen" style="background-image: url();">
+    <div className="hero-overlay bg-opacity-60"></div>
+    <div className="card w-96 glass">
+      <figure><img className="" src="" alt="krystopher quintero"/></figure>
+      <div className="card-body">
+        <h2 className="card-title">CONTACT INFO</h2>
+        <p>EMAIL: krystopher.q@gmail.com</p>
+        <p>PHONE: (562)-552-7768</p>
+        <p>GITHUB: <a className="link link-hover" href='https://github.com/KrystopherQ' target="_blank">PROFILE</a></p>
+        <p>LINKEDIN: <a className="link link-hover" href="https://www.linkedin.com/in/krystopher-quintero-955320238/" target="_blank">PROFILE</a></p>
+      
+      </div>
+    </div>
+      </div>
+    </div>
+  )}
+  export default Contact;
